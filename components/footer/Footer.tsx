a54371d17ad4ea347baf056cb013614a130596bf
Marin Aeschbach <marin.aeschbach@gmail.com>
2023-02-10 13:14:33 +0100

    LIPPMANN-10: Fix artwork card shadows

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import styles from './Footer.module.scss'
-import { ActiveLink } from '../ActiveLink'
+import React from 'react';
+import styles from './Footer.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export default function Footer() {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li className={styles.navItem}>
-          <ActiveLink
-            href="/a-propos"
-            activeClassName={styles.active}
-          >
-            Á propos
-          </ActiveLink>
-        </li>
-        <li className={styles.navItem}>
-          <ActiveLink
-            href="/gabriel-lippmann"
-            activeClassName={styles.active}
-          >
-            Gabriel Lippmann
-          </ActiveLink>
-        </li>
-      </ul>
-    </nav>
-  )
+    <footer>
+      <nav className={styles.nav}>
+        <ul>
+          <li className={styles.navItem}>
+            <ActiveLink
+              href="/a-propos"
+              className={styles.navLink}
+              activeClassName={styles.active}
+            >
+              Á propos
+            </ActiveLink>
+          </li>
+          <li className={styles.navItem}>
+            <ActiveLink
+              href="/gabriel-lippmann"
+              className={styles.navLink}
+              activeClassName={styles.active}
+            >
+              Gabriel Lippmann
+            </ActiveLink>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 }
